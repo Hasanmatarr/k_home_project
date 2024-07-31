59b42d478cd8ef9b919fa704ff92316d15c7820d
Hasanmatarr <hasanmatarux@gmail.com>
2024-07-31 19:37:23 +0300

    second commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,8 +4,12 @@ class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
 
+  final String backgroundImageUrl =
+      'https://cdn.pixabay.com/photo/2021/03/01/13/22/housewares-6059709_1280.jpg';
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffod;
   }
 }
